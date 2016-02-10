e902b7449b74879966bb637a2a7f55b3bb530f1a
gugarutz <sebastian.bartmann@gmail.com>
2016-02-11 00:30:39 +0100

    beep?

--- beepoler/beep.py
+++ beepoler/beep.py
@@ -3,37 +3,43 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-BeepPin = 11    # pin11
+BeepPin = 11  # pin11
 dummy = 1
 
+
 def setup():
-    GPIO.setmode(GPIO.BOARD)        # Numbers GPIOs by physical location
-    GPIO.setup(BeepPin, GPIO.OUT)   # Set BeepPin's mode is output
-    GPIO.output(BeepPin, GPIO.HIGH) # Set BeepPin high(+3.3V) to off beep
+    GPIO.setmode(GPIO.BOARD)  # Numbers GPIOs by physical location
+    GPIO.setup(BeepPin, GPIO.OUT)  # Set BeepPin's mode is output
+    GPIO.output(BeepPin, GPIO.HIGH)  # Set BeepPin high(+3.3V) to off beep
+
 
 def loop():
     while True:
-        if dummy == 1:
-            GPIO.output(BeepPin, GPIO.LOW)
-            time.sleep(0.1)
-            GPIO.output(BeepPin, GPIO.HIGH)
-            time.sleep(0.1)
-        else:
-            time.sleep(0.4)
+        # if dummy == 1:
+        GPIO.output(BeepPin, GPIO.LOW)
+        time.sleep(0.1)
+        GPIO.output(BeepPin, GPIO.HIGH)
+        time.sleep(0.1)
+        # else:
+        time.sleep(0.4)
+
 
 def destroy():
-    GPIO.output(BeepPin, GPIO.HIGH)    # beep off
-    GPIO.cleanup()                     # Release resource
+    GPIO.output(BeepPin, GPIO.HIGH)  # beep off
+    GPIO.cleanup()  # Release resource
 
-if __name__ == '__main__':     # Program start from here
+
+if __name__ == '__main__':  # Program start from here
     print 'Press Ctrl+C to end the program...'
     setup()
     try:
+        loop()
+
         for line in sys.stdin:
             if line == "ON":
                 dummy = 1
             else:
                 dummy = 0
-        loop()
+
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
         destroy()
