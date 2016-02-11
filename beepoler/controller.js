6b6ff242e2a9cc6999c6c651fdf9a3bb5139a3a4
gugarutz <sebastian.bartmann@gmail.com>
2016-02-11 01:22:00 +0100

    beep?

--- beepoler/controller.js
+++ beepoler/controller.js
@@ -1,26 +1,45 @@
-
-var PythonShell = require('python-shell');
+//var PythonShell = require('python-shell');
 var express = require('express');
 var app = express();
 /*
-PythonShell.run('beep.py', function (err) {
-    if (err) throw err;
-    console.log('finished');
-});*/
+ PythonShell.run('beep.py', function (err) {
+ if (err) throw err;
+ console.log('finished');
+ });*/
 
-var pyshell = new PythonShell('beep.py');
+//var pyshell = new PythonShell('beep.py');
+
+var spawn = require('child_process').spawn;
+var py = spawn('python', ['beep.py']);
+
+/*
+ py.stdout.on('data', function(data){
+ dataString += data.toString();
+ });
+ py.stdout.on('end', function(){
+ console.log('Sum of numbers=',dataString);
+ });
+ */
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.post('/on', function (req, res) {
-    pyshell.send('ON');
+app.get('/on', function (req, res) {
+    //pyshell.send('ON');
+
+    py.stdin.write('ON');
+    py.stdin.end();
+
     console.log("da!");
 });
 
-app.post('/off', function (req, res) {
-    pyshell.send('OFF');
+app.get('/off', function (req, res) {
+    //pyshell.send('OFF');
+
+    py.stdin.write('OFF');
+    py.stdin.end();
+
     console.log("da2!");
 });
 
@@ -35,50 +54,50 @@ PythonShell.run('beep.py', function (err) {
 
 
 /*
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var routes = require('./routes')(io);
-var PythonShell = require('python-shell');
+ var express = require('express');
+ var app = express();
+ var bodyParser = require('body-parser');
+ var server = require('http').Server(app);
+ var io = require('socket.io')(server);
+ var routes = require('./routes')(io);
+ var PythonShell = require('python-shell');
 
-// initialize body-parser
-// bodyparser parses the body from a HTTP POST to a JSON object
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.json());
+ // initialize body-parser
+ // bodyparser parses the body from a HTTP POST to a JSON object
+ //app.use(bodyParser.urlencoded({extended: true}));
+ //app.use(bodyParser.json());
 
-var port = process.env.PORT || 1337;
+ var port = process.env.PORT || 1337;
 
-var router = express.Router();
+ var router = express.Router();
 
-// middleware for all requests
-// if there is a new request, it has to pass this middleware
-// POST to /api/message -> this function -> router.route(/message)
-// GET to /api/message -> this function -> router.route(/message)
-// just for i dont know ... logging?
-router.use(function (req, res, next) {
-    // ToDo: Logging.
+ // middleware for all requests
+ // if there is a new request, it has to pass this middleware
+ // POST to /api/message -> this function -> router.route(/message)
+ // GET to /api/message -> this function -> router.route(/message)
+ // just for i dont know ... logging?
+ router.use(function (req, res, next) {
+ // ToDo: Logging.
 
-    console.log('new request.');
-    next(); // dont stop, go to the next route
-})
+ console.log('new request.');
+ next(); // dont stop, go to the next route
+ })
 
-// initialize root route (0.0.0.0:1337/api)
-router.get('/', routes.index);
+ // initialize root route (0.0.0.0:1337/api)
+ router.get('/', routes.index);
 
-// initialize message route (0.0.0.0:1337/api/message)
-router.route('/beepon')
-    .post(routes.on);
+ // initialize message route (0.0.0.0:1337/api/message)
+ router.route('/beepon')
+ .post(routes.on);
 
-router.route('/beepoff')
-    .post(routes.off);
+ router.route('/beepoff')
+ .post(routes.off);
 
-// define that /api is the root route
-app.use('/api', router);
+ // define that /api is the root route
+ app.use('/api', router);
 
-// start server
-server.listen(port);
+ // start server
+ server.listen(port);
 
-console.log('texta api started on port ' + port);
-    */
+ console.log('texta api started on port ' + port);
+ */
