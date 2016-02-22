35f4a7c89aa31118f7ad39c48805d2e697df6eea
gugarutz <sebastian.bartmann@gmail.com>
2016-02-22 02:33:32 +0100

    other method testing

--- ledcontrol/maled.py
+++ ledcontrol/maled.py
@@ -27,7 +27,7 @@ def getrandomcolor(red, green, blue, a, b):
 
 def test(strip):
     for i in range(strip.numPixels()):
-        strip.setPixelColor(i, 100, 0, 0)
+        strip.setPixelColor(i, Color(100, 0, 0))
         strip.show()
         time.sleep(50 / 1000.0)
 
