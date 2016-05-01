c863aa8638b2b7e87fb899b7e54252fc5080eaa8
gugarutz <sebastian.bartmann@gmail.com>
2016-05-01 17:05:24 +0200

    new test / learn file

---
+++ ledcontrol/ledLearna.py
@@ -0,0 +1,42 @@
+import time
+import random
+
+from neopixel import *
+
+# LED strip configuration:
+LED_COUNT = 60  # Number of LED pixels.
+LED_PIN = 18  # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 5  # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+FULL_COLOR = 255 # maximum brightness of one rgb color part
+
+
+def testlearna():
+    strip.setPixelColor(1, Color(255,255,255))
+
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, color)
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, 0)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print 'Press Ctrl-C to quit.'
+    while True:
+        testlearna()
+        #theaterChase(strip, Color(235,   91,   14), 200)
